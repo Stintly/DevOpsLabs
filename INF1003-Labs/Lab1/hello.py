7b5f90f25e100650dade583659ca59bc9710e6e6
Ryo <ryoseahhx@gmail.com>
2026-09-04 16:53:40 +0800

    lab 1 done

--- INF1003-Labs/Lab1/hello.py
+++ INF1003-Labs/Lab1/hello.py
@@ -1,1 +1,29 @@
+print("===================")
 print("Hello")
+print("===================")
+
+username = input("Enter your username: ")
+bio = "i'm a crumb"
+followers = 100
+age = input("Enter your age: ")
+category = input("Enter your category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print ("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
+print ("Bio: ", bio)
+print ("Followers: ", followers)
+
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print ("Day 2:", followers)
+
+followers -= 10
+print ("Day 3:", followers)
+
+if age>60 and category=="Lifestyle":
+    print("You are a senior lifestyle influencer!")
